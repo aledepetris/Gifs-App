84ebef379252fb41beb4442551f9afe31e06f042
Alejandro Depetris <aledepetris@outlook.com>
2023-02-17 14:59:40 -0300

    Se agregaron validaciones a la busqueda (blancos y repetidos), se muestra en title case los resultados, se muestran las ultimas 10 busquedas.

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,15 +9,19 @@ export class BusquedaComponent implements OnInit {
 
   @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
+  constructor (private gifsService: GifsService ) {}
+
   buscar() {
 
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor)
+
+    if ( valor.trim().length === 0 ) {return;}
+
+    this.gifsService.buscarGifs( valor )
+
     this.txtBuscar.nativeElement.value = '';
 
   }
-
-  constructor() { }
 
   ngOnInit(): void {
   }
