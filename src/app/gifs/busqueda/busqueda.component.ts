91c023544b160e772fa34a987879e4295c7d97aa
Alejandro Depetris <aledepetris@outlook.com>
2023-02-17 14:31:28 -0300

    Se utilizo @ViewChild para acceder a un Input html y obtener lo que se escriba en el mismo y borrar su contenido posteriormente

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,10 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
 })
 export class BusquedaComponent implements OnInit {
+
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  buscar() {
+
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor)
+    this.txtBuscar.nativeElement.value = '';
+
+  }
 
   constructor() { }
 
