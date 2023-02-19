e95dac393ea3e019f354cef4f3dad57d6d2b4b39
Alejandro Depetris <aledepetris@outlook.com>
2023-02-19 12:23:41 -0300

    Se creó el componente de resultados, se agrego el service para comunicarse con la API de gifs. Se muestran los resultados en el componente. App funcionando

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,21 +6,35 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey    : string = '1AabxaL9DczIfTIfmzb6Lmv0jms71FBs';
   private _historial: string[] = [];
+
+  // TODO: Cambiar el tipo de datos.
+  public resultados : any[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
+  constructor ( private http: HttpClient ) { }
+
   buscarGifs( query: string ) {
 
-    if ( !this._historial.includes( query )) {
-      query = query.trim().toLocaleLowerCase();
-      this._historial.unshift( query );
-      this._historial = this._historial.splice(0, 10)
+    query = query.trim().toLocaleLowerCase();
+
+    const index = this._historial.indexOf(query);
+    if (index !== -1) {
+      this._historial.splice(index, 1);
     }
 
-    console.log(this._historial)
+    this._historial.unshift( query );
+    this._historial = this._historial.splice(0, 10);
+
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=1AabxaL9DczIfTIfmzb6Lmv0jms71FBs&q=${query}&limit=25&offset=0&rating=g&lang=en`)
+      .subscribe( (response:any) => {
+        console.log( response.data );
+        this.resultados = response.data;
+      } )
 
   }
 
